d2380d300f490766b0f4dfb1b5040ec514958fce
Mohamed Fadel <32733023+wmfadel@users.noreply.github.com>
2019-07-08 02:31:29 +0200

    add file to VCS

---
+++ lib/src/widgets/series_top_rated.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:provider/provider.dart';
+import '../providers/tv_provider.dart';
+import '../widgets/tv_item.dart';
+
+class SeriesTopRated extends StatefulWidget {
+  @override
+  _SeriesTopRatedState createState() => _SeriesTopRatedState();
+}
+
+class _SeriesTopRatedState extends State<SeriesTopRated> {
+  TVProvider provider;
+  double screenWidth;
+  double screenHeight;
+  ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels >=
+          _scrollController.position.maxScrollExtent)
+        setState((){
+          Provider.of<TVProvider>(context).getTopRatedTv();
+        });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    screenHeight = size.height;
+    screenWidth = size.width;
+    provider = Provider.of<TVProvider>(context);
+
+    return FutureBuilder(
+      future: provider.getTopRatedTv(),
+      builder: (context, snapshot) {
+        if (provider.topRated.length > 0 &&
+            snapshot.data != null &&
+            snapshot.data)
+          return Container(
+            height: 310,
+            width: screenWidth,
+            child: ListView.builder(
+              key: PageStorageKey('providerpopulartv'),
+              controller: _scrollController,
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemCount: provider.topRated.length,
+              itemBuilder: (context, index) {
+                return TVItem(provider.topRated[index]);
+              },
+            ),
+          );
+        return Center(
+          child: SpinKitWave(
+            color: Colors.white,
+            size: 35,
+          ),
+        );
+      },
+    );
+  }
+
+}
