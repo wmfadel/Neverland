f9ee55d2812d2bb001d92d2013a7e6898aabd954
Mohamed Fadel <32733023+wmfadel@users.noreply.github.com>
2019-08-14 21:34:57 +0200

    Update detailed_tv_provider.dart

--- lib/src/providers/detailed_tv_provider.dart
+++ lib/src/providers/detailed_tv_provider.dart
@@ -8,7 +8,7 @@ import '../models/tv_details.dart';
 
 class DetailedTvProvider with ChangeNotifier {
   final String _baseUrl = 'https://api.themoviedb.org/3/';
-  final String _key = 'api_key=f05286ad9b97b7821731e08bc891a337';
+  final String _key = 'api_key=ADD_YOUR_OWN_KEY';
   bool isLoading = false;
   TVDetails _tvDetails;
 
